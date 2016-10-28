57caebecd33d25fe9a49a183d67b91f7d5904a4c
Ilya Radchenko <ilya@burstcreations.com>
2016-10-28 17:34:39 -0400

    Add override areas comp

--- app/components/override-area.js
+++ app/components/override-area.js
@@ -1,3 +1,27 @@
-import OverrideArea from 'ember-admin/components/override-area';
+import Ember from 'ember';
 
-export default OverrideArea;
+const { set, inject } = Ember;
+
+const Component = Ember.Component.extend({
+  adminConfig: inject.service(),
+
+  didReceiveAttrs() {
+    let areas = this.get('adminConfig.areas');
+    let area = this.get('area');
+    let type = this.get('type') || 'default';
+    let send = this.get('send');
+    let location = `${area}.${type}`;
+
+    if (!areas[area]) {
+      areas[area] = Ember.Object.create();
+    }
+
+    set(areas, location, send);
+  }
+});
+
+Component.reopenClass({
+  positionalParams: ['area', 'type']
+});
+
+export default Component;
