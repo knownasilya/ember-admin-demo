2253c4205419d4aee9147c37fde865b3ce598fac
Ilya Radchenko <ilya@burstcreations.com>
2016-11-17 10:24:04 -0500

    Don't use an action for theme change

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,14 +5,6 @@ const { inject } = Ember;
 export default Ember.Controller.extend({
   queryParams: ['selectedTheme'],
   adminConfig: inject.service(),
-  selectedTheme: 'default',
   themes: ['default', 'bootstrap'],
-  actions: {
-    changeTheme(theme) {
-      this.setProperties({
-        selectedTheme: theme,
-        'adminConfig.activeTheme': theme
-      });
-    }
-  }
+  selectedTheme: 'default'
 });
